ee84545469f71bb70d5a76f7bf632dbb86070bd9
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2024-03-05 01:53:05 +0800

    Adds test and script for hit method
    Add script that logs hits to a ship object

--- src/apps/createShip.js
+++ src/apps/createShip.js
@@ -1,12 +1,16 @@
 class Ship {
     constructor (length) {
         this.length = length;
-        this.hits = 0;
+        this.hitPoints = 0;
         this.sank = false;
     }
 
+    hit() {
+        this.hitPoints += 1;
+    }
+
     checkSank () {
-        if (this.length === this.hits) {
+        if (this.length === this.hitPoints) {
             this.sank = true;
         }
     }
--- test/battleship.test.js
+++ test/battleship.test.js
@@ -1,11 +1,21 @@
 import { Ship } from "../src/apps/createShip";
 
 test ('Can create ships', () => {
-    const newShip = new Ship(4);
+  const newShip = new Ship(4);
 
-    expect(newShip).toEqual({
-        length: 4,
-        hits: 0,
-        sank: false,
-    })
+  expect(newShip).toEqual({
+      length: 4,
+      hitPoints: 0,
+      sank: false,
+  })
+})
+
+test ('Ship gets hits', () => {
+  const newShip = new Ship(2);
+
+  newShip.hit(); 
+  expect(newShip.hitPoints).toBe(1);
+
+  newShip.hit();
+  expect(newShip.hitPoints).toBe(2);
 })
